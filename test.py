a466c6b01dd98277ab93e971f5d6c6875cccc7cd
SmithLeroy <1773185860@qq.com>
2017-09-29 11:46:20 +0800

    logistic regression conpleted

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import logRegres
-
+from numpy import *
 
 dataArr, labelMat = logRegres.loadDataSet()
-weights = logRegres.gradAscent(dataArr, labelMat)
-print weights
+# weights1 = logRegres.gradAscent(dataArr, labelMat)
+# print weights1
+#
+# logRegres.plotBestFit(weights1.getA())
+#
+# weights2 = logRegres.stocGradAscent(array(dataArr), labelMat)
+# logRegres.plotBestFit(weights2)
 
-logRegres.plotBestFit(weights.getA())
+# weights3 = logRegres.strocGradAscent1(dataArr, labelMat)
+# print weights3
+# logRegres.plotBestFit(weights3)
+
+logRegres.multiTest()
+
+
 
